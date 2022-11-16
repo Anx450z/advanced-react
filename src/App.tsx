a875710a67735762c6d90348db9a10fd3af714e0
Ankur <anx450z@gmail.com>
2022-11-16 11:04:57 +0530

    add types and props

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import NewNote from "./NewNote";
 
+export type Note = {
+  id: string
+} & NoteData // adds NoteData type to Note type
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
 function App() {
   return (
     <Container className="my-4">
