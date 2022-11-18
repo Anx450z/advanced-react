bdfe86bdd248d3e01b4171655b1022b28f1b3cda
Ankur <anx450z@gmail.com>
2022-11-18 11:54:20 +0530

    addded markdown

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import NewNote from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
 import { v4 as uuidV4 } from "uuid";
 import NoteList from "./NoteList";
+import NoteLayout from "./NoteLayout";
+import Note from "./Note";
 
 export type Note = {
   id: string;
@@ -62,7 +64,10 @@ function App() {
     <Container className="my-4">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags} />} />
+          <Route
+            path="/"
+            element={<NoteList notes={notesWithTags} availableTags={tags} />}
+          />
           <Route
             path="/new"
             element={
@@ -73,8 +78,8 @@ function App() {
               />
             }
           />
-          <Route path="/:id">
-            <Route index element={<h1>Show</h1>} />
+          <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
+            <Route index element={<Note />} />
             <Route path="edit" element={<h1>Edit</h1>} />
           </Route>
           <Route path="*" element={<h1>Page does not exist</h1>} />
